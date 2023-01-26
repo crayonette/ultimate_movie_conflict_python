94abe6ab77a2a2c88bebcbe92712312370f10ce9
Sandra Furmanova <110247492+crayonette@users.noreply.github.com>
2023-01-26 15:14:07 +0300

    Initial commit

---
+++ test_movie_conflict.py
@@ -0,0 +1,7 @@
+from selene.support.shared import browser
+from selene import have, be
+
+
+def test_google_good_search(open_browser):
+    browser.element('[id=suggestion-search]').should(be.blank).type('batman 1989').press_enter()
+
